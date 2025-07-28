8bd301d878a0438e9d0f76915badf276fc00568e
Kudae Sithu <kudaesithu2@gmail.com>
2025-07-28 17:26:23 +0630

    add axios for API calls and implement SuperHeros component to fetch and display data

--- src/components/SuperHeros.jsx
+++ src/components/SuperHeros.jsx
@@ -1,6 +1,40 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+
 const SuperHeros = () => {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+  
+  useEffect(() => {
+    const fetchSuperHeros = () => {
+      axios.get("http://localhost:3000/superheroes")
+        .then(response => {
+          console.log(response.data);
+          setData(response.data);
+          setLoading(false);
+        })
+        .catch(error => {
+          console.error("Error fetching superheroes:", error);
+        });
+    }
+
+    fetchSuperHeros();
+  }, []);
+
   return (
-    <div>SuperHeros</div>
+    <div> 
+      <p>Traditional SuperHeros</p>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {data.map(hero => (
+            <li key={hero.id}>{hero.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
   )
 }
 
