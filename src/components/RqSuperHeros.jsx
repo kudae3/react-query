26c8e2ab84f6328ce09909566c6491b82c01a200
Kudae Sithu <kudaesithu2@gmail.com>
2025-07-29 12:06:59 +0630

    refactor RqSuperHeros component to integrate useAddSuperHero hook for adding new superheroes; remove unused refetch button

--- src/components/RqSuperHeros.jsx
+++ src/components/RqSuperHeros.jsx
@@ -1,16 +1,43 @@
-import { useFetchSuperHeros } from "../hooks/useFetchSuperHeros";
+import { useState } from "react";
+import { useAddSuperHero, useFetchSuperHeros } from "../hooks/useFetchSuperHeros";
+
+
 
 const RqSuperHeros = () => {
 
-  const {isLoading, isFetching, error, data, refetch} = useFetchSuperHeros();
-  console.log({isFetching, isLoading});
+  const {isLoading, isFetching, error, data} = useFetchSuperHeros();
+  const AddSuperHeroMutation = useAddSuperHero();
   
+  const [name, setName] = useState('');
+  const [alterEgo, setAlterEgo] = useState('');
+
+  console.log({isFetching, isLoading});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    AddSuperHeroMutation.mutate({ name, alterEgo });
+  }
 
   return (
     <div>
       <p>React Query SuperHeros</p>
       {error && <p>Error: {error.message}</p>}
-      <button onClick={refetch}>Refetch Heros</button>
+      {/* <button onClick={refetch}>Refetch Heros</button> */}
+      
+      
+      <form onSubmit={handleSubmit} method="post">
+        
+        <input onChange={(e) => {
+          setName(e.target.value);
+        }} value={name} type="text" name="name" placeholder="Hero Name" />
+
+        <input onChange={(e) => {
+          setAlterEgo(e.target.value);
+        }} value={alterEgo} type="text" name="alterEgo" placeholder="Hero Alter Ego" />
+
+        <button type="submit">Add Hero</button>
+      </form>
+      
       {
         (isLoading || isFetching) && <p>Loading...</p>
       }
--- src/hooks/useFetchSuperHeros.js
+++ src/hooks/useFetchSuperHeros.js
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { QueryClient, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 
 const fetchSuperHeros = async() => {
@@ -10,6 +10,24 @@ export const useFetchSuperHeros = () => {
     return useQuery({
         queryKey: ['super-heroes'],
         queryFn: fetchSuperHeros,
-        enabled: false
+        // enabled: false
     });
+}
+
+export const addSuperHero = async({ name, alterEgo }) => {
+    const response = await axios.post('http://localhost:3000/superheroes', {
+        name,
+        alterEgo
+    });
+    return response.data;
+}
+
+export const useAddSuperHero = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: addSuperHero,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['super-heroes'] });
+        }
+    })
 }
